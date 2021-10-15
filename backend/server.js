218111bb62ec90de243686bf8c521ae9e6741eeb
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-10-15 17:19:22 +0530

    removed favicon
    removed faviconn

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,15 @@ app.get('/api/config/razorpay', (req, res) => res.send(process.env.PAYMENT_ID))
 // const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
   )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
 app.use(notFound)
