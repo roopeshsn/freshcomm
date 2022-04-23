1bd387bbda185587cce6a7ae6063fc84c8d08389
Felexonyango <felexonyango19@gmail.com>
2022-04-23 13:19:28 +0300

    product route done  test case okay

--- test/productController.test.js
+++ test/productController.test.js
@@ -22,10 +22,86 @@ describe('PRODUCT TESTS', () => {
          })
 
      })
-    
-
 
     })
+    describe("/ GET /api/products/:id",()=>{
+        it("should return product by id", (done) => {
+            const id = 1;
+            chai.request(app)
+            .get(`/api/products${id}`)
+            .end((err, res)=>{ 
+               res.should.have.a('object')
+               done()
+            })
+   
+        })
+   
+       })
+       describe("/  DELETE /api/products/:id",()=>{
+        it("should return product by id", (done) => {
+            const id = 1;
+            chai.request(app)
+            .delete(`/api/products${id}`)
+            .end((err, res)=>{ 
+               res.should.have.a('object')
+               done()
+            })
+   
+        })
+   
+       })
+       describe("/ POST /api/products/",()=>{
+        it("should create a product", (done) => {
+            let products =new Product({
+                name:"Banana",
+                price:20,
+                mrp:1,
+                user: 1,
+                imageSrc:"jkdekde",
+                imageAlt:"kdieduied",
+                category :"kdidldil",
+                countInStock:2,
+                description : "Test sweet",
+
+            })
+            chai.request(app)
+            .post("/api/products")
+            .send(products)
+            .end((err, res)=>{ 
+               res.should.have.a('object')
+               done()
+            })
+   
+        })
+   
+       })
+       describe("/ PUT /api/products/:id",()=>{
+        it("should update a product", (done) => {
+        let id =1
+        let products =new Product({
+            name:"orange",
+            price:27,
+            mrp:2,
+            user: 1,
+            imageSrc:"jkdekdess",
+            imageAlt:"kdieduiedcsxdsx",
+            category :"kdidldilxs",
+            countInStock:3,
+            description : "Test sweeter",
+
+        })
+            chai.request(app)
+            .put(`/api/products/${id}`)
+            .send(products)
+            .end((err, res)=>{ 
+               res.should.have.a('object')
+               done()
+            })
+   
+        })
+   
+       })
+   
 
 
 })
