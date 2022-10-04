4ab0eeeab04f2d0ddfcc41bf4b9b23466f98e831
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2022-10-04 20:16:18 +0530

    cleanup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -26,14 +26,11 @@ const Header = () => {
             <Navbar.Brand id="logo-text">Freshbey</Navbar.Brand>
           </LinkContainer>
           <Nav className="nav-right">
-            <Nav.Link
-              href="//api.whatsapp.com/send?phone=+916369984456&text=help"
-              target="_blank"
-            >
+            <Nav.Link href="#" target="_blank">
               <BsWhatsapp size="1.5em" />
             </Nav.Link>
 
-            <Nav.Link href="tel:+91-6369984456">
+            <Nav.Link href="#">
               <FiPhoneCall size="1.5em" />
             </Nav.Link>
 
