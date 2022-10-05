43aa5f58aa871eb7c9a59ea836c4b4921fc8e808
masterHAWK99 <maciej.pochron99@gmail.com>
2022-10-05 15:55:08 +0200

    Improve footer section #72

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,49 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Nav } from 'react-bootstrap'
 
 const Footer = () => {
   return (
     <footer>
       <Container>
-        <Row>
+        <Row className="flex-column flex-lg-row">
+          <Col className="py-3">
+            <ul className="list-unstyled d-flex li-separator justify-content-center align-items-center">
+              <li className="d-flex px-1">
+                <Nav.Link className="p-0" href="#" target="_blank">
+                  Returns Policy
+                </Nav.Link>
+              </li>
+              <li className="d-flex px-1">
+                <Nav.Link className="p-0" href="#" target="_blank">
+                  Terms of Use
+                </Nav.Link>
+              </li>
+              <li>
+                <Nav.Link className="p-0" href="#" target="_blank">
+                  Privacy Policy
+                </Nav.Link>
+              </li>
+            </ul>
+          </Col>
           <Col className="text-center py-3">Copyright &copy; FreshBey</Col>
+          <Col className="text-center py-3 d-flex justify-content-center">
+            Need help? Visit the
+            <Nav.Link
+              className="py-0 px-1 text-decoration-underline"
+              href="#"
+              target="_blank"
+            >
+              Help Center
+            </Nav.Link>
+            or
+            <Nav.Link
+              className="py-0 px-1 text-decoration-underline"
+              href="#"
+              target="_blank"
+            >
+              Contact Us
+            </Nav.Link>
+          </Col>
         </Row>
       </Container>
     </footer>
